5ad13394d05be73732a7e6b6ce2ebe2d9f6b0e2d
codewithaatif <querywithatif@gmail.com>
2026-07-13 23:13:00 +0530

    Basic concept done

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log("Working")
+
+// const div = document.getElementById('div')
+
+
+// const button = document.getElementById('button')
+// const secondBox = document.getElementById('second')
+
+// let val
+// button.addEventListener('click', () => {
+//     const input = document.getElementById('input-text');
+//     console.log(input.value);
+//     val = input.value
+//     let outputText = val
+
+//     for(let i=0; i<5; i++){ 
+//         outputText += val
+//     }
+
+//     secondBox.innerText = `${outputText}`
+// })
+
+
+
+// console.log(input.value);    
+// console.log(div);
+
+const finalButton = document.getElementById('third')
+
+finalButton.addEventListener('click', () => {
+    const userText = document.getElementById('input-text')
+    const times = document.getElementById('times')
+
+    let displayText = userText.value
+
+    for(let i=0; i<times.value; i++){
+        displayText += " " + userText.value
+    }
+
+    const displayBox = document.getElementById('third')
+    // displayBox.innerText = `${displayText}`
+    displayBox.innerHTML = `<p id="para">${displayText}</p>`
+    document.getElementById('para').style.backgroundColor = "blue"
+})
+
