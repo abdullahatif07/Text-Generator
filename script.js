88cb58855daaf4d4a2584c596e6fb77cb5022484
codewithaatif <querywithatif@gmail.com>
2026-07-14 23:33:20 +0530

    Some changes

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log("Working")
-
 // const div = document.getElementById('div')
-
 
 // const button = document.getElementById('button')
 // const secondBox = document.getElementById('second')
@@ -20,18 +18,42 @@ console.log("Working")
 //     secondBox.innerText = `${outputText}`
 // })
 
-
-
 // console.log(input.value);    
 // console.log(div);
 
 const finalButton = document.getElementById('third')
 
-finalButton.addEventListener('click', () => {
+// const submitButton = document.getElementById('submit-button')
+
+// submitButton.addEventListener('click', () => {
+//     const userText = document.getElementById('input-text')
+//     const times = document.getElementById('times')
+
+//     let displayText = userText.value
+
+//     for(let i=0; i<times.value; i++){
+//         displayText += " " + userText.value
+//     }
+
+//     const displayBox = document.getElementById('third')
+//     // displayBox.innerText = `${displayText}`
+//     displayBox.innerHTML = `<p id="para">${displayText}</p>`
+//     const para = document.getElementById('para')
+//     para.style.backgroundColor = "rgb(50, 87, 84)"
+//     // para.style.backgroundColor = "yellow"
+//     para.style.padding = "10px"
+//     para.style.borderRadius = "10px"
+// })
+
+
+const timesButton = document.getElementById('times')
+
+timesButton.addEventListener('input', () => {
     const userText = document.getElementById('input-text')
     const times = document.getElementById('times')
 
-    let displayText = userText.value
+    // let displayText = userText.value
+    let displayText = ""
 
     for(let i=0; i<times.value; i++){
         displayText += " " + userText.value
@@ -40,6 +62,10 @@ finalButton.addEventListener('click', () => {
     const displayBox = document.getElementById('third')
     // displayBox.innerText = `${displayText}`
     displayBox.innerHTML = `<p id="para">${displayText}</p>`
-    document.getElementById('para').style.backgroundColor = "blue"
+    const para = document.getElementById('para')
+    para.style.backgroundColor = "rgb(50, 87, 84)"
+    // para.style.backgroundColor = "yellow"
+    para.style.padding = "10px"
+    para.style.borderRadius = "10px"
 })
 
