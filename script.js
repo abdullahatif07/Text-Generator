8a43e2b9911610ad1e29e032d18621da34460669
codewithaatif <querywithatif@gmail.com>
2026-07-15 21:04:49 +0530

    Completed almost

--- script.js
+++ script.js
@@ -1,71 +1,33 @@
-console.log("Working")
-// const div = document.getElementById('div')
-
-// const button = document.getElementById('button')
-// const secondBox = document.getElementById('second')
-
-// let val
-// button.addEventListener('click', () => {
-//     const input = document.getElementById('input-text');
-//     console.log(input.value);
-//     val = input.value
-//     let outputText = val
-
-//     for(let i=0; i<5; i++){ 
-//         outputText += val
-//     }
-
-//     secondBox.innerText = `${outputText}`
-// })
-
-// console.log(input.value);    
-// console.log(div);
-
 const finalButton = document.getElementById('third')
-
-// const submitButton = document.getElementById('submit-button')
-
-// submitButton.addEventListener('click', () => {
-//     const userText = document.getElementById('input-text')
-//     const times = document.getElementById('times')
-
-//     let displayText = userText.value
-
-//     for(let i=0; i<times.value; i++){
-//         displayText += " " + userText.value
-//     }
-
-//     const displayBox = document.getElementById('third')
-//     // displayBox.innerText = `${displayText}`
-//     displayBox.innerHTML = `<p id="para">${displayText}</p>`
-//     const para = document.getElementById('para')
-//     para.style.backgroundColor = "rgb(50, 87, 84)"
-//     // para.style.backgroundColor = "yellow"
-//     para.style.padding = "10px"
-//     para.style.borderRadius = "10px"
-// })
-
-
 const timesButton = document.getElementById('times')
 
+let displayText = ""
+
 timesButton.addEventListener('input', () => {
+    displayText = ""
+
     const userText = document.getElementById('input-text')
     const times = document.getElementById('times')
 
-    // let displayText = userText.value
-    let displayText = ""
-
     for(let i=0; i<times.value; i++){
-        displayText += " " + userText.value
+        displayText += userText.value + " "
     }
 
     const displayBox = document.getElementById('third')
-    // displayBox.innerText = `${displayText}`
-    displayBox.innerHTML = `<p id="para">${displayText}</p>`
-    const para = document.getElementById('para')
-    para.style.backgroundColor = "rgb(50, 87, 84)"
-    // para.style.backgroundColor = "yellow"
-    para.style.padding = "10px"
-    para.style.borderRadius = "10px"
+
+    const outputText = document.getElementById('output-text')
+    outputText.innerText = `${displayText}`
 })
 
+const copyButton = document.getElementById('copy-button')
+const toast = document.getElementById('toast')
+
+copyButton.addEventListener('click', async () => {
+    await navigator.clipboard.writeText(displayText)
+    console.log("HI")
+    toast.classList.add("show")
+
+    setTimeout(() => {
+        toast.classList.remove("show")
+    }, 2000)
+});
